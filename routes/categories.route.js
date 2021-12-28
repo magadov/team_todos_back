9b4bd30df9fdc590fbb8994842f379ad3a8fbb30
usman <dzakaev.94@mail.ru>
2021-12-28 14:23:22 +0300

    'финишбэк'

---
+++ routes/categories.route.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+const {
+  categoriesController,
+} = require("../controllers/categories.controller");
+
+const router = Router();
+
+router.get("/", categoriesController.getAll);
+router.get("/:id", categoriesController.getCategoryById);
+router.post("/", categoriesController.createCategory);
+router.delete("/:id", categoriesController.deleteCategory);
+router.patch("/:id", categoriesController.editCategory);
